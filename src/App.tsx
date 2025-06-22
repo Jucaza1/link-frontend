271dcf568e695364eb48d83547ec93be59ff7e40
Juan Cano <juan01cz@gmail.com>
2025-06-22 23:20:28 +0200

    fix: modifying navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes, BrowserRouter } from 'react-router'
 import './App.css'
-import Default from './pages/Default'
 import Home from './pages/Home'
 import { Navbar } from './components/Navbar'
 import { Links } from './pages/Links'
 import { UserContextProvider } from './context/UserContextProvider'
 import { Toaster } from 'react-hot-toast'
 import { Profile } from './pages/Profile'
+import { About } from './pages/About'
 
 function App() {
 
@@ -16,12 +16,12 @@ function App() {
                 <BrowserRouter>
                     <div className="w-full min-h-screen flex flex-col bg-gray-100 ">
                         <Navbar />
-                        <div className="flex-1/2 max-w-10/12 self-center w-full p-6 bg-white shadow-md mx-auto h-full">
+                        <div className="flex-1/2 max-w-10/12 self-center w-full bg-white shadow-md mx-auto h-full">
                             <Routes>
                                 <Route path="/" Component={Home} />
                                 <Route path="/links" Component={Links} />
                                 <Route path="/profile" Component={Profile} />
-                                <Route path="/default" Component={Default} />
+                                <Route path="/about" Component={About} />
                             </Routes>
                         </div>
                     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,59 @@
 // lets create a Navbar component
 import { Link, NavLink } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
+import { useContext } from 'react';
+import { UserContext } from '@/context/UserContext';
+import { purgeLocalStorage } from '@/util/helpers';
 
 
 export function Navbar() {
+    const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+    const handleLogout = (event: React.MouseEvent) => {
+        event.preventDefault()
+        purgeLocalStorage()
+        setIsLoggedIn(false)
+        window.location.href = '/'
+    }
     return (
-        <nav className="bg-white drop-shadow-lg">
+        <nav className="bg-white drop-shadow-lg pb-2">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
+                <div className="flex justify-between h-16 gap-8 items-center">
                     <div className="flex items-center">
                         {/* Logo or Brand Name , with animation on the lowdash*/}
-                        <Link to="/" className="text-3xl font-bold text-gray-900">LinkShare<span className="text-lime-500 animate-caret-blink">__</span></Link>
+                        <Link to="/" className="text-3xl font-bold text-gray-900">LinkShare<span className="font-extrabold text-5xl text-lime-500 animate-caret-blink">_</span></Link>
                     </div>
-                    <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-4 gap-3">
                         {/* underline where the user is currently at */}
                         <NavLink to="/"
                             className={({ isActive }) =>
-                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                                isActive ? "px-2 border-b-2 border-lime-500 text-gray-700 hover:text-gray-900"
+                                    : "px-2 text-gray-700 hover:text-gray-900"
                             }
                         >Home</NavLink>
                         <NavLink to="/links"
                             className={({ isActive }) =>
-                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                                isActive ? "px-2 border-b-2 border-lime-500 text-gray-700 hover:text-gray-900"
+                                    : "px-2 text-gray-700 hover:text-gray-900"
                             }
                         >Links</NavLink>
                         <NavLink to="/profile"
                             className={({ isActive }) =>
-                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                                isActive ? "px-2 border-b-2 border-lime-500 text-gray-700 hover:text-gray-900"
+                                    : "px-2 text-gray-700 hover:text-gray-900"
                             }
                         >Profile</NavLink>
+                        <NavLink to="/about"
+                            className={({ isActive }) =>
+                                isActive ? "px-2 border-b-2 border-lime-500 text-gray-700 hover:text-gray-900"
+                                    : "px-2 text-gray-700 hover:text-gray-900"
+                            }
+                        >About</NavLink>
                         <Button variant="outline" asChild>
-                            <Link to="/login">Login</Link>
+                            {isLoggedIn ? (
+                                <Link onClick={handleLogout} to="/" className="bg-red-300">Log out</Link>
+                            ) : (
+                                <Link to="/">Log in</Link>
+                            )}
                         </Button>
                     </div>
                 </div>
