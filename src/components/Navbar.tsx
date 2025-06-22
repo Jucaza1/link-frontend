20f3d4b8daaae757f7c47411aee8fdb70c130e3e
Juan Cano <juan01cz@gmail.com>
2025-06-22 06:40:22 +0200

    feat: modifying navbar to use NavLink instead of Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,34 @@
 // lets create a Navbar component
-import { Link } from 'react-router-dom';
-import { cn } from '@/lib/utils';
+import { Link, NavLink } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
 
 export function Navbar() {
     return (
-        <nav className="bg-white shadow-md">
+        <nav className="bg-white drop-shadow-lg">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex items-center">
-                        <Link to="/" className="text-xl font-bold text-gray-900">LinkEase</Link>
+                        {/* Logo or Brand Name , with animation on the lowdash*/}
+                        <Link to="/" className="text-3xl font-bold text-gray-900">LinkShare<span className="text-lime-500 animate-caret-blink">__</span></Link>
                     </div>
                     <div className="flex items-center space-x-4">
-                        <Link to="/" className={cn("text-gray-700 hover:text-gray-900")}>Home</Link>
-                        <Link to="/default" className={cn("text-gray-700 hover:text-gray-900")}>Default</Link>
+                        {/* underline where the user is currently at */}
+                        <NavLink to="/"
+                            className={({ isActive }) =>
+                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                            }
+                        >Home</NavLink>
+                        <NavLink to="/links"
+                            className={({ isActive }) =>
+                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                            }
+                        >Links</NavLink>
+                        <NavLink to="/profile"
+                            className={({ isActive }) =>
+                                isActive ? "border-b-2 border-lime-500 text-gray-700 hover:text-gray-900" : "text-gray-700 hover:text-gray-900"
+                            }
+                        >Profile</NavLink>
                         <Button variant="outline" asChild>
                             <Link to="/login">Login</Link>
                         </Button>
